f690de872d7f1bf91f50d3110306a96a15593964
Patina Makasheva <patinamakasheva@gmail.com>
2024-06-01 00:57:34 +0300

    hw done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("exceptions");
+
+        String login = "sky_pro";
+        String password = "Dnfddsf_5";
+        String confirmPassword = "Dnfddsf_";
+
+        Credential credential = new Credential(login, password);
+        try {
+            CredentialValidator.validateCredentials(credential, confirmPassword);
+            System.out.println("Логин и пароль успешно прошли проверку.");
+        } catch (WrongLoginException | WrongPasswordException e) {
+            System.out.println("Ошибка проверки логина и пароля: " + e.getMessage());
+        }
     }
 }
